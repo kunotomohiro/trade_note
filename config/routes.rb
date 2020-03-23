cb8bad1375e0b0bedf4ec305b13414db8e700e5c
kunotomohiro <kunotomohiro215@gmail.com>
2020-03-24 04:51:37 +0900

    active_strage_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
   resources :static_pages
+  namespace :user do
+    resource :user_profile
+    resources :trades do
+    end
+  end
 end
