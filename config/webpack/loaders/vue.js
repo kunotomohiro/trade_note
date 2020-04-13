73c86adbdfc09a7b8f67ed1301c5fdb7a8c3d627
久野知広 <kunotomohiro215@gmail.com>
2020-04-13 07:52:21 +0000

    Revert "色々設定してみた (pull request #150)"
    This reverts pull request #150.
    
    > 色々設定してみた

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -2,13 +2,5 @@ module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
     loader: 'vue-loader'
-  }],
-  test: /\.css$/,
-  use: [{
-    loader: 'style-loader!css-loader'
-  }],
-  test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
-  use: [{
-    loader: 'url-loader'
   }]
 }
