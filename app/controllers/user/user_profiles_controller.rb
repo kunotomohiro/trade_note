e69ee884134ebde969a694889f6897426127e4c6
kunotomohiro <kunotomohiro215@gmail.com>
2020-04-14 19:28:49 +0900

    vueでuser_profile作れるようにした

--- app/controllers/user/user_profiles_controller.rb
+++ app/controllers/user/user_profiles_controller.rb
@@ -1,15 +1,21 @@
 class User::UserProfilesController < User::ApplicationController
+  protect_from_forgery :except => [:create, :update]
 
   def new
     @user_profile = UserProfile.new
   end
 
   def create
-    @user_profile = current_user.build_user_profile(user_profile_params)
-    if @user_profile.save
-      redirect_to user_trades_path
-    else
-      render "new"
+    @user_profile = current_user.build_user_profile(user_profile_params.except(:avatar))
+    @user_profile.base64upload(user_profile_params[:avatar])
+    respond_to do |format|
+      if @user_profile.save
+        format.html
+        format.json { render json: @user_profile, status: :created}
+      else
+        format.html
+        format.json { render json: @user_profile.errors, status: :unprocessable_entity }
+      end
     end
   end
 
