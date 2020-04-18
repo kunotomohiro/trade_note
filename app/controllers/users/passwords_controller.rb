a5240cf0ce396d31ac83bfd827b87480adff03f4
kunotomohiro <kunotomohiro215@gmail.com>
2020-04-19 00:28:40 +0900

    header作成

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  layout 'user/application.html.erb'
   # GET /resource/password/new
   # def new
   #   super
