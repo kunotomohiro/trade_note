8fd1e93e9f66a525eb6ffab3f3115547884a6318
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-03 00:53:00 +0900

    passwordをmixinにまとめた

--- app/javascript/packs/user/password_for_user.js
+++ app/javascript/packs/user/password_for_user.js
@@ -1,19 +1,13 @@
 import Vue from 'vue/dist/vue.esm.js'; 
+import password from "../mixin/user/password.js"
 
 window.onload = function(){
   var passwordForUser = new Vue({
     el: "#password_for_user",
+    mixins: [password],
     data: {
       email: "",
-      password: "",
-      passwordConfirmation: "",
-      isPush: true,
-      passwordLength: false,
-      characterCase:  false,
-      includeNumber:  false,
-      emailStatus:    false,
-      passwordStatus: false,
-      passwordConfirmationStatus: false
+      emailStatus:    false
     },
     created: function() {
       self = this
@@ -26,38 +20,6 @@ window.onload = function(){
           self.emailStatus = false
         }
         self.toggleIspush()
-      },
-      inputPassword() {
-
-        self.password.length >= 8 ? self.passwordLength = true : self.passwordLength = false
-
-        self.password.match(/(?=.*?[a-z])(?=.*?[A-Z])/) ? self.characterCase = true : self.characterCase = false
-
-        self.password.match(/\d/) ? self.includeNumber = true : self.includeNumber = false
-
-        if (self.passwordLength === true && self.characterCase === true && self.includeNumber === true){
-          self.passwordStatus = true
-        } else {
-          self.passwordStatus = false
-        }
-        
-        self.inputPasswordConfirmation()
-        self.toggleIspush()
-      },
-      inputPasswordConfirmation() {
-        if (self.passwordConfirmation === self.password && self.passwordConfirmation.length >= 1 ) {
-          self.passwordConfirmationStatus = true
-        } else {
-          self.passwordConfirmationStatus = false
-        }
-        self.toggleIspush()
-      },
-      toggleIspush() {
-        if (self.emailStatus === true && self.passwordStatus === true && self.passwordConfirmationStatus === true ) {
-          self.isPush = false
-        } else {
-          self.isPush = true
-        }
       }
     }
   })
