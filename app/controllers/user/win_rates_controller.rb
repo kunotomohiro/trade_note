aca486ebcce0f0122c99bf42d0babdaf0398a4e4
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-02 19:49:23 +0900

    勝率の表示非表示実装

--- app/controllers/user/win_rates_controller.rb
+++ app/controllers/user/win_rates_controller.rb
@@ -1,7 +1,6 @@
 class User::WinRatesController < User::ApplicationController
 
   def index
-    @trade = Trade.new(current_user_id: current_user.id)
     @fx = Fx.new(current_user_id: current_user.id)
     @stock = Stock.new(current_user_id: current_user.id)
     @virtual_currency = VirtualCurrency.new(current_user_id: current_user.id)
