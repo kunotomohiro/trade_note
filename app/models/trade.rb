eece949208157e917434e98cff945af2a556a9c8
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-11 22:08:08 +0900

    年内のトレード勝率を表示できるようにした

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -50,4 +50,29 @@ class Trade < ApplicationRecord
     "#{(trades / user_trades.count.to_f * 100).floor(1)}%"
   end
 
+  def search_trades_in_year
+    from  = Time.now.at_beginning_of_year
+    to    = Time.now.at_end_of_year
+    Trade.where(exit_time: from...to)
+  end
+
+  def user_trades_in_year
+    search_trades_in_year.where("(user_id = ?) and (trade_category_id = ?)", current_user_id, trade_category_id)
+  end
+
+  def win_number_of_trades_in_year
+    user_trades_in_year.where("result" => "資産増")
+  end
+
+  def total_win_rate_in_year
+    return "0%" if win_number_of_trades_in_year.count === 0
+    "#{(win_number_of_trades_in_year.count / user_trades_in_year.count.to_f * 100).floor(1)}%"
+  end
+
+  def trade_style_win_rate_in_year(trade_style_id)
+    return "0%" if win_number_of_trades_in_year.count === 0
+    trades = win_number_of_trades_in_year.where("trade_style_id" => trade_style_id).count
+    "#{(trades / user_trades_in_year.count.to_f * 100).floor(1)}%"
+  end
+
 end
