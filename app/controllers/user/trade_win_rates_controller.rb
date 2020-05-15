ebfb152445f96c21b5dd75eca16825d41e4e3115
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-15 18:37:03 +0900

    yearの条件分岐をTime型ni

--- app/controllers/user/trade_win_rates_controller.rb
+++ app/controllers/user/trade_win_rates_controller.rb
@@ -1,7 +1,7 @@
 class User::TradeWinRatesController < User::ApplicationController
 
   def index
-    year              = params[:trade_win_rate].nil? ? 2020 : params[:trade_win_rate][:year]
+    year              = params[:trade_win_rate].nil? ? Time.now.strftime("%Y") : params[:trade_win_rate][:year]
     @fx               = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 1, year: year)
     @stock            = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 2, year: year)
     @virtual_currency = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 3, year: year)
