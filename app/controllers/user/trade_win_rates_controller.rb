105b441f2a5ecaf895254132404b972f61032c2b
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-20 18:26:57 +0900

    月のごとの勝率を出せるようにした

--- app/controllers/user/trade_win_rates_controller.rb
+++ app/controllers/user/trade_win_rates_controller.rb
@@ -1,10 +1,15 @@
 class User::TradeWinRatesController < User::ApplicationController
 
   def index
-    year              = params[:trade_win_rate].nil? ? Time.now.strftime("%Y") : params[:trade_win_rate][:year]
+    year              = params[:trade_win_rate].nil? || params[:trade_win_rate][:year].nil? ? Time.now.strftime("%Y") : params[:trade_win_rate][:year]
+    month_rate_year   = params[:trade_win_rate].nil? || params[:trade_win_rate][:month_rate_year].nil? ? Time.now.strftime("%Y") : params[:trade_win_rate][:month_rate_year]
+    month             = params[:trade_win_rate].nil? || params[:trade_win_rate][:month].nil? ? Time.now.strftime("%m").delete("0") : params[:trade_win_rate][:month]
     @fx               = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 1, year: year)
     @stock            = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 2, year: year)
     @virtual_currency = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 3, year: year)
+    @fx_in_month      = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 1, month_rate_year: month_rate_year, month: month)
+    @stock_in_month   = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 2, month_rate_year: month_rate_year, month: month)
+    @virtual_currency_in_month = TradeWinRate.new(current_user_id: current_user.id, trade_category_id: 3, month_rate_year: month_rate_year, month: month)
   end
 
 end
--- app/models/trade_win_rate.rb
+++ app/models/trade_win_rate.rb
@@ -21,4 +21,15 @@ class TradeWinRate < Trade
     trades = win_number_of_trades_in_year.where("trade_style_id" => trade_style_id).count
     "#{(trades / user_trades_in_year.count.to_f * 100).floor(1)}%"
   end
+
+  def total_win_rate_in_month
+    return "0%" if win_number_of_trades_in_month.count === 0
+    "#{(win_number_of_trades_in_month.count / user_trades_in_month.count.to_f * 100).floor(1)}%"
+  end
+
+  def trade_style_win_rate_in_month(trade_style_id)
+    return "0%" if win_number_of_trades_in_month.count === 0
+    trades = win_number_of_trades_in_month.where("trade_style_id" => trade_style_id).count
+    "#{(trades / user_trades_in_month.count.to_f * 100).floor(1)}%"
+  end
 end
