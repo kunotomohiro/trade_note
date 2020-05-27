b639f93a72ebf4c9eb7d37a4b7e5f9a1ded9b76d
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-28 01:01:44 +0900

    ユーザーがログインするところまでの統合テスト書いた

--- spec/features/trades_spec.rb
+++ spec/features/trades_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Trades", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "ユーザーは新しいトレードを作成する" do
+    user = FactoryBot.create(:user)
+    visit root_path
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    click_button "ログイン"
+    expect(current_path).to eq user_trades_path
+  end
+  # pending "add some scenarios (or delete) #{__FILE__}"
 end
