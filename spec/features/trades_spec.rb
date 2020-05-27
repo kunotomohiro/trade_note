8d92888e56f1a36702e3d499c61ef4f981353ac0
kunotomohiro <kunotomohiro215@gmail.com>
2020-05-27 23:14:06 +0900

    trades_spec.rbファイルのの作成

--- spec/features/trades_spec.rb
+++ spec/features/trades_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.feature "Trades", type: :feature do
-  scenario "ユーザーは新しいトレードを作成する" do
-    user = FactoryBot.create(:user)
-    visit root_path
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
-    click_button "Log in"
-
-
-  end
-  # pending "add some scenarios (or delete) #{__FILE__}"
+  pending "add some scenarios (or delete) #{__FILE__}"
 end
